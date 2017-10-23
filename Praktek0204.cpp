309c80a436b4d8dd1163593ba63e6a23b3424e5e
kendi ardi <arsamisapper12@gmail.com>
2017-10-23 08:03:07 +0800

    Cout Luas 1 Box Parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -5,5 +5,6 @@ int main(){
 	double LuasRmh = 42;
 	double LntP    = 193*1215;
 	double LuasBox = LntP*8/1000000;
-		cout << "Luas Rumah		: "<<LuasRmh<<" m2"<<endl;
+		cout << "Luas Rumah		   : "<<LuasRmh<<" m2"<<endl;
+		cout << "Luas 1 Box parket : "<<LuasBox<<" m2"<<endl;
 }
