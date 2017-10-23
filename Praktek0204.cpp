f554245b964df7012963fbf4b749ea83b88ccb7c
kendi ardi <arsamisapper12@gmail.com>
2017-10-23 08:04:02 +0800

    Perbaikan

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	double LuasRmh = 42;
 	double LntP    = 193*1215;
-	double LuasBox = LntP*8/1000000;
+	double Luas1Box = LntP*8/1000000;
 		cout << "Luas Rumah		   : "<<LuasRmh<<" m2"<<endl;
-		cout << "Luas 1 Box parket : "<<LuasBox<<" m2"<<endl;
+		cout << "Luas 1 Box parket : "<<Luas1Box<<" m2"<<endl;
 }
