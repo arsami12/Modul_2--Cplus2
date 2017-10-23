5380a5bcbb1b0fdb96d8c67bd47a0b0b21485345
kendi ardi <arsamisapper12@gmail.com>
2017-10-23 08:21:19 +0800

    Biaya Paket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -8,5 +8,8 @@ int main(){
 		cout << "Luas Rumah		   : "<<LuasRmh<<" m2"<<endl;
 		cout << "Luas 1 Box parket : "<<Luas1Box<<" m2"<<endl;
 	double JmlBoxP,BiayaP,BiayaPG,TotlB;
-			JmlBoxP = LuasRmh/Luas1Box;
+		   JmlBoxP = LuasRmh/Luas1Box;
+		cout << "Jumlah Box Parket		: "<<JmlBoxP<<endl;
+			BiayaP = JmlBoxP*500;
+	
 }
